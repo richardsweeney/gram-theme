3d06fb642aec77b4459b543dd9a97de155ac3170
Jessica Martin <I>
2016-08-23 16:08:09 +0200

    fixed frontpage image

--- views/common/frontpage-sections/intro.blade.php
+++ views/common/frontpage-sections/intro.blade.php
@@ -8,9 +8,10 @@
   </div>
 
   @if( has_post_thumbnail() )
-  <div id="js-parallax-background" class="parallax-background" style="background: url('{{ the_post_thumbnail_url() }}')"></div>
+  <div id="js-parallax-background" class="parallax-background" style="background: url('{{ the_post_thumbnail_url() }}') 0% 0% / cover;"></div>
 	@endif
 
   <a href="#section-features" class="jump-down"><i class="fa fa-chevron-down"></i></a>
 
 </div>
+
