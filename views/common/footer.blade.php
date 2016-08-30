ca6b06da0bc2441772f1c932f662bc2e958279b1
Richard Sweeney <theorobman@gmail.com>
2016-08-30 20:46:41 +0200

    More fixes for the landing page

--- views/common/footer.blade.php
+++ views/common/footer.blade.php
@@ -1,20 +1,21 @@
 <footer class="footer" role="contentinfo">
-  	<div class="footer-logo">
-      {{ dynamic_sidebar( 'footer-1' ) }}
- 	</div>
+    <div class="footer-inner">
+        <div class="footer-logo">
+          <?php dynamic_sidebar( 'footer-1' ) ?>
+        </div>
 
-    <ul class="footer-social">
-      {{ dynamic_sidebar( 'footer-2' ) }}
-    </ul>
-
+        <div class="footer-social">
+            <?php dynamic_sidebar( 'footer-2' ) ?>
+        </div>
+    </div>
 </footer>
 
 <footer class="footer-2" role="contentinfo">
-    <ul>
-      {{ wp_nav_menu([ 'theme_location' => 'footer' ]) }}
-    </ul>
+    <div class="footer-inner">
+        {{ wp_nav_menu([ 'theme_location' => 'footer' ]) }}
 
-    <div class="copyright-info">
-      <p class="pull-right"> Gram Malmö &copy; <?php echo date('Y'); ?> <?php echo the_author_link(); ?></p>
+        <div class="copyright-info">
+            <p class="pull-right"> Gram Malmö &copy; <?php echo date('Y'); ?> <?php echo the_author_link(); ?></p>
+        </div>
     </div>
 </footer>
--- views/common/frontpage-sections/about-home.blade.php
+++ views/common/frontpage-sections/about-home.blade.php
@@ -1,14 +1,12 @@
 <section id="about" class="about-home-wrapper">
-
-	<div class="mg"></div>
-    <div class="about-text-right">
-        <h3>{{ $about_home_title }}</h3>
-        <p class="text">{{ $about_home_text }}</p>
-    </div>
-	<div class="image-left">
+    <div class="image-left">
         @if ( $about_home_image )
             <img src="{{ $about_home_image }}" alt="">
         @endif
+    </div>
+    <div class="about-text-right">
+        <h3>{{ $about_home_title }}</h3>
+        <p class="text">{{ $about_home_text }}</p>
     </div>
 </section>
 
