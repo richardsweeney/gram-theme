32f96ec393e2485da2bad12f6459f55abdc8587b
Richard Sweeney <theorobman@gmail.com>
2016-10-16 18:29:53 +0200

    Fix search template

--- views/search.blade.php
+++ views/search.blade.php
@@ -4,23 +4,27 @@
 
     <section class="posts-list">
 
-        <header>
-            <h1>{{ __( 'Search results for' ) }} <span class="search-term">"{{ the_search_query() }}"</span></h1>
-        </header>
+        <div class="container">
 
-        <div class="search-results-container">
+            <header>
+                <h1>{{ __( 'Search results for' ) }} <span class="search-term">"{{ the_search_query() }}"</span></h1>
+            </header>
 
-            @if( have_posts() )
-                @while( have_posts() )
-                    {{ the_post() }}
-                    @include( 'views.posts.excerpt' )
-                @endwhile
-            @else
-                <p>{{ __( "No results found. Maybe you'd like to try searching again?", 'gram' ) }}</p>
-                <div class="search-form-container">
-                    {!! get_search_form( false ) !!}
-                </div>
-            @endif
+            <div class="search-results-container">
+
+                @if( have_posts() )
+                    @while( have_posts() )
+                        {{ the_post() }}
+                        @include( 'views.posts.excerpt' )
+                    @endwhile
+                @else
+                    <p>{{ __( "No results found. Maybe you'd like to try searching again?", 'gram' ) }}</p>
+                    <div class="search-form-container">
+                        {!! get_search_form( false ) !!}
+                    </div>
+                @endif
+
+            </div>
 
         </div>
 
